d7a45851362582da43bd1aca72a41350830387f1
Adam Wead <amsterdamos@gmail.com>
2015-02-08 10:45:12 -0800

    Ruby script to batch convert xml

---
+++ convert.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+require 'byebug'
+
+docs = [
+  'cpb-aacip-500-6h4csj1p_pbcore.xml',
+  'cpb-aacip-500-6m335r61_pbcore.xml'  
+]
+xslt  = Nokogiri::XSLT(File.read('pbcore-2-ebucore.xsl'))
+
+docs.each do |xml_doc|
+  doc = Nokogiri::XML(File.read(xml_doc))
+  rdf_name = File.basename(xml_doc).split(/\./).first+("_rdf.xml")
+  rdf_path = File.join("rdf_xml", rdf_name)
+  f = File.open(rdf_path, "w")
+  f.write(xslt.transform(doc))
+  f.close
+end
