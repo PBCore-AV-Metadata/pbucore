07f1311e9e2542a91d4478fd2bde908184402027
Chuck McCallum <chuck_mccallum@wgbh.org>
2015-04-30 15:48:43 -0400

    Make sure the fixtures directory is clean.

--- spec/convert_spec.rb
+++ spec/convert_spec.rb
@@ -2,12 +2,18 @@ require 'nokogiri'
 require_relative '../lib/converter'
 
 describe 'converter' do
-  Dir[File.dirname(__FILE__) + '/fixtures/*.pbcore.xml'].each do |pbcore_path|
+  inputs = Dir[File.dirname(__FILE__) + '/fixtures/*.pbcore.xml']
+  inputs.each do |pbcore_path|
     rdf_path = pbcore_path.sub('.pbcore.xml', '.rdf.xml')
     it "converts #{File.basename(pbcore_path)} to #{File.basename(rdf_path)}" do
       rdf_xml_actual = Converter.instance.convert(pbcore_path).to_s
       rdf_xml_expected = File.read(rdf_path)
       expect(rdf_xml_actual).to eq rdf_xml_expected
     end
+  end
+  
+  it 'has no stray fixtures' do
+    all = Dir[File.dirname(__FILE__) + '/fixtures/*']
+    expect(all.count).to eq inputs.count * 2
   end
 end
